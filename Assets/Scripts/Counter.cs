70165c51fb7e6dcb3d46e20e98ec34577302eeb8
bibinba <qin4869ryu@gmail.com>
2020-07-23 21:43:57 +0900

    実装
    Signed-off-by: bibinba <qin4869ryu@gmail.com>

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -1,47 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class Counter : MonoBehaviour
 {
 
-public UnityEngine.UI.Text TimeText;
+    public Text Text_mmsss;    
+    public Text Text_ff;                         //デジタルテキストの取得用変数
+    private float movetime;                          //稼働時間用変数
+    private bool moving;                             //ストップウォッチ稼働フラグ
 
-public static float countTime;
-
-private int a;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        countTime = 0;
-        a = 0;
-    }
-
-    // Update is called once per frame
+void Start(){
+    Text_mmsss.text = "00:00";
+    Text_ff.text = ".00";
+}
     void Update()
     {
-        if(a == 1){
-           countTime += Time.deltaTime; 
+        //スタートボタンを押したら稼働する
+        if (moving)
+        {
+            //ストップウォッチ稼働時間
+            movetime += Time.deltaTime;
+            //デジタルテキスト表示
+            TimeSet();
         }
-        if(a == 2){
-           countTime = countTime; 
-        }
-        if(a == 0){
-           countTime = 0; 
-        }
-
-    TimeText.text = countTime.ToString ("F2");
- 
     }
 
-   public void OnClickStart(){
-        a = 1;
-   }
-   public void OnClickStop(){
-        a = 2;
-   }
-   public void OnClickReset(){
-        a = 0;
-   }
+
+    //各ボタンの機能
+    //ストップウォッチのスタート
+    public void WatchStart()
+    {
+        moving = true;
+    }
+    //ストップウォッチのストップ
+    public void WatchStop()
+    {
+        moving = false;
+    }
+    //ストップウォッチのリセット
+    public void TimeReset()
+    {
+        //稼働中はリセットを無効にする
+        if (moving)
+        {
+            return;
+        }
+
+        movetime = 0;
+        Text_mmsss.text = "00:00";
+        Text_ff.text = ".00";
+    }
+
+
+    //時・分・秒の表示方法を指定
+    private void TimeSet()
+    {
+        //各種変数
+        string timetext_mmss = null;
+        int provtime = 0;
+        int twodecimal = 0;
+
+        //稼働時間から小数点以下を取り出して二桁の整数にする
+        provtime = Mathf.FloorToInt(movetime);
+        twodecimal = Mathf.FloorToInt((movetime - provtime) * 100);
+
+        //TimeSpanを使って　00：00：00.　の文字列を作る
+        TimeSpan ts = new TimeSpan(0, 0, provtime);
+        Text_mmsss.text = ts.ToString(@"mm\:ss");
+        //切り離した小数点以下を文字化する
+        Text_ff.text = "."+twodecimal.ToString("D2");
+
+    }
 }
