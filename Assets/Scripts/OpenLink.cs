d828776356f10f1a883122407fda7650ef06f94f
bibinba <qin4869ryu@gmail.com>
2020-07-25 16:29:50 +0900

    素材追加
    Signed-off-by: bibinba <qin4869ryu@gmail.com>

---
+++ Assets/Scripts/OpenLink.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void Link(string url)
+    {
+#if UNITY_EDITOR
+        Application.OpenURL(url);
+#elif UNITY_WEBGL
+    Application.ExternalEval(string.Format("window.open('{0}','_blank')", url));
+#else
+    Application.OpenURL(url);
+#endif
+    }
+}
